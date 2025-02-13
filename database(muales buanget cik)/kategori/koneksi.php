290d355c990d0338fe2ca86f17b50ec9f939f487
SukaMCD <fabian25march@gmail.com>
2025-02-13 09:02:29 +0700

    Delete database(muales buanget cik) directory

--- database(muales buanget cik)/kategori/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$koneksi = mysqli_connect("localhost","root","","crud");
-
-if (mysqli_connect_errno()) {
-    echo "koneksi Database Gagal : " . mysqli_connect_errno();
-}
-
-?>
--- database(muales buanget cik)/produk/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "leaflytea");
-
-//check connection
-if (mysqli_connect_errno()) {
-    echo "Koneksi Database Gagal : " . mysqli_connect_error();
-}
